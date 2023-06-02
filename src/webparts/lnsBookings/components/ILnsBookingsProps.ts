b202610a57f6ed151fb750e9172abd40ab9e2706
NunoFl <39694398+NunoFl@users.noreply.github.com>
2023-06-02 15:42:49 +0100

    backup

---
+++ src/webparts/lnsBookings/components/ILnsBookingsProps.ts
@@ -0,0 +1,7 @@
+export interface ILnsBookingsProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/lnsBookings/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface ILnsBookingsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'LnsBookingsWebPartStrings' {
+  const strings: ILnsBookingsWebPartStrings;
+  export = strings;
+}
