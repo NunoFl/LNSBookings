2703b87f36f3dc9b9cc67a8daa708d19d1815d75
NunoFl <39694398+NunoFl@users.noreply.github.com>
2023-06-02 17:00:15 +0100

    errors persisting on render

--- src/webparts/lnsBookings/components/LnsBookings.tsx
+++ src/webparts/lnsBookings/components/LnsBookings.tsx
@@ -6,12 +6,37 @@ import { escape } from '@microsoft/sp-lodash-subset';
 
 
 
+//Criar objecto para eventos, pessoas e popular o ecrã com essa dummy date.
+function getCalendarBooking() {
+
+  // Calendar booking object
+  const [booking, setBooking] = useState({});
+  getCalendarBooking()
+  console.log('teste ---', booking)
+  setBooking('')
+}
+
+
+useEffect(() => {
+  const booking =
+  {
+    id: 1,
+    owner: 'nuno.florido@lisbonnearshore.com',
+    details: {
+      days: ['11/06/2023', '12/06/2023'],
+      invitees: {
+        1: { email: 'external@hotmail.com', confirmed: false },
+        2: { email: 'jose.fraga@lisbonnearshore.com', confirmed: false },
+        3: { email: 'vanessa.velosa@lisbonnearshore.com', confirmed: false }
+      }
+    }
+  }
+   // get events from sharepoint data to state
+}, [])
+
 
 
 export default class LnsBookings extends React.Component<ILnsBookingsProps, {}> {
-
-
-
 
   public render(): React.ReactElement<ILnsBookingsProps> {
     const {
@@ -23,60 +48,36 @@ export default class LnsBookings extends React.Component<ILnsBookingsProps, {}> 
     } = this.props;
 
 
-    //Criar objecto para eventos, pessoas e popular o ecrã com essa dummy date.
-    function getCalendarBooking() {
-      // Calendar booking object
-      const [booking, setBooking] = useState({
-        id: 1,
-        owner: 'nuno.florido@lisbonnearshore.com',
-        details: {
-          days: ['11/06/2023', '12/06/2023'],
-          invitees: {
-            1: { email: 'external@hotmail.com', confirmed: false },
-            2: { email: 'jose.fraga@lisbonnearshore.com', confirmed: false },
-            3: { email: 'vanessa.velosa@lisbonnearshore.com', confirmed: false }
-          }
-        }
-      });
-      console.log('teste ---', booking)
-
-      setBooking(booking)
-      // Rest of the code...
-    }
-
-    getCalendarBooking()
-
-    useEffect(() => {
-      //get events
-
-    }, [])
 
 
     return (
-      <section className={`${styles.lnsBookings} ${hasTeamsContext ? styles.teams : ''}`}>
-        <div className={styles.welcome}>
-          <img alt="" src={isDarkTheme ? require('../assets/welcome-dark.png') : require('../assets/welcome-light.png')} className={styles.welcomeImage} />
-          <h2>Well done, {escape(userDisplayName)}!</h2>
-          <div>{environmentMessage}</div>
-          <div>Web part property value: <strong>{escape(description)}</strong></div>
-        </div>
-        <div>
-          <h3>Welcome to SharePoint Framework!</h3>
-          <p>
-            The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It&#39;s the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
-          </p>
-          <h4>Learn more about SPFx development:</h4>
-          <ul className={styles.links}>
-            <li><a href="https://aka.ms/spfx" target="_blank" rel="noreferrer">SharePoint Framework Overview</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-graph" target="_blank" rel="noreferrer">Use Microsoft Graph in your solution</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-teams" target="_blank" rel="noreferrer">Build for Microsoft Teams using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-viva" target="_blank" rel="noreferrer">Build for Microsoft Viva Connections using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-store" target="_blank" rel="noreferrer">Publish SharePoint Framework applications to the marketplace</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-api" target="_blank" rel="noreferrer">SharePoint Framework API reference</a></li>
-            <li><a href="https://aka.ms/m365pnp" target="_blank" rel="noreferrer">Microsoft 365 Developer Community</a></li>
-          </ul>
-        </div>
-      </section>
+      <>
+      {getCalendarBooking}
+        <section className={`${styles.lnsBookings} ${hasTeamsContext ? styles.teams : ''}`}>
+          <div className={styles.welcome}>
+            <img alt="" src={isDarkTheme ? require('../assets/welcome-dark.png') : require('../assets/welcome-light.png')} className={styles.welcomeImage} />
+            <h2>Well done, {escape(userDisplayName)}!</h2>
+            <div>{environmentMessage}</div>
+            <div>Web part property value: <strong>{escape(description)}</strong></div>
+          </div>
+          <div>
+            <h3>Welcome to SharePoint Framework!</h3>
+            <p>
+              The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It&#39;s the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
+            </p>
+            <h4>Learn more about SPFx development:</h4>
+            <ul className={styles.links}>
+              <li><a href="https://aka.ms/spfx" target="_blank" rel="noreferrer">SharePoint Framework Overview</a></li>
+              <li><a href="https://aka.ms/spfx-yeoman-graph" target="_blank" rel="noreferrer">Use Microsoft Graph in your solution</a></li>
+              <li><a href="https://aka.ms/spfx-yeoman-teams" target="_blank" rel="noreferrer">Build for Microsoft Teams using SharePoint Framework</a></li>
+              <li><a href="https://aka.ms/spfx-yeoman-viva" target="_blank" rel="noreferrer">Build for Microsoft Viva Connections using SharePoint Framework</a></li>
+              <li><a href="https://aka.ms/spfx-yeoman-store" target="_blank" rel="noreferrer">Publish SharePoint Framework applications to the marketplace</a></li>
+              <li><a href="https://aka.ms/spfx-yeoman-api" target="_blank" rel="noreferrer">SharePoint Framework API reference</a></li>
+              <li><a href="https://aka.ms/m365pnp" target="_blank" rel="noreferrer">Microsoft 365 Developer Community</a></li>
+            </ul>
+          </div>
+        </section>
+      </>
     );
   }
 }
