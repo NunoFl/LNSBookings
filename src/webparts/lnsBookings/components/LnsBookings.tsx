e0cd06a79d4de8a31a2737be5e700ad8d0f97ca9
NunoFl <39694398+NunoFl@users.noreply.github.com>
2023-06-02 15:45:46 +0100

    add dummy data

--- src/webparts/lnsBookings/components/LnsBookings.tsx
+++ src/webparts/lnsBookings/components/LnsBookings.tsx
@@ -1,9 +1,18 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import styles from './LnsBookings.module.scss';
 import { ILnsBookingsProps } from './ILnsBookingsProps';
 import { escape } from '@microsoft/sp-lodash-subset';
 
+
+
+
+
 export default class LnsBookings extends React.Component<ILnsBookingsProps, {}> {
+
+
+
+
   public render(): React.ReactElement<ILnsBookingsProps> {
     const {
       description,
@@ -12,6 +21,36 @@ export default class LnsBookings extends React.Component<ILnsBookingsProps, {}> 
       hasTeamsContext,
       userDisplayName
     } = this.props;
+
+
+    //Criar objecto para eventos, pessoas e popular o ecrã com essa dummy date.
+    function getCalendarBooking() {
+      // Calendar booking object
+      const [booking, setBooking] = useState({
+        id: 1,
+        owner: 'nuno.florido@lisbonnearshore.com',
+        details: {
+          days: ['11/06/2023', '12/06/2023'],
+          invitees: {
+            1: { email: 'external@hotmail.com', confirmed: false },
+            2: { email: 'jose.fraga@lisbonnearshore.com', confirmed: false },
+            3: { email: 'vanessa.velosa@lisbonnearshore.com', confirmed: false }
+          }
+        }
+      });
+      console.log('teste ---', booking)
+
+      setBooking(booking)
+      // Rest of the code...
+    }
+
+    getCalendarBooking()
+
+    useEffect(() => {
+      //get events
+
+    }, [])
+
 
     return (
       <section className={`${styles.lnsBookings} ${hasTeamsContext ? styles.teams : ''}`}>
