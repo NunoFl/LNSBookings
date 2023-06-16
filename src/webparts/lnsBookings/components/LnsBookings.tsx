23eb3382a8212501f965bbfbe0c2fd504e4cc8e2
NunoFl <39694398+NunoFl@users.noreply.github.com>
2023-06-16 15:34:22 +0100

    calendar injection sucessful, some styles, needs control to handle rendering on press next and previous week

--- src/webparts/lnsBookings/components/LnsBookings.tsx
+++ src/webparts/lnsBookings/components/LnsBookings.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 //@ts-ignore
 import { useState, useEffect } from 'react';
-import styles from './LnsBookings.module.scss';
+
 import { ILnsBookingsProps } from './ILnsBookingsProps';
 import { escape } from '@microsoft/sp-lodash-subset';
 // @ts-ignore
@@ -9,13 +9,136 @@ import { SPHttpClient, SPHttpClientResponse } from '@microsoft/sp-http';
 import { NormalPeoplePicker } from '@fluentui/react/lib/Pickers';
 import { IPersonaProps } from 'office-ui-fabric-react';
 
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+// @ts-ignore
+import customViewPlugin from './CustomView.js';
+
+import styles from './LnsBookings.module.scss';
+import './index.scss';
+
+function LnsCalendar() {
+  // const [selectedDates, setSelectedDates] = useState<Date>([]);
+  // @ts-ignore
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const calendarRef = React.useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // Simulating an asynchronous rendering process
+    const delay = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    if (calendarRef.current) {
+      const calendar = new Calendar(
+        calendarRef.current, {
+        plugins: [dayGridPlugin, customViewPlugin],
+        selectable: true,
+        initialView: 'dayGridWeek',
+        now: new Date(),
+        weekends: false,
+        visibleRange: {
+          start: 'now',
+          end: new Date().setDate(new Date().getDate() + 15)
+        },
+      });
+
+      calendar.render();
+      dayCellContent();
+      return () => {
+        calendar.destroy();
+        clearTimeout(delay); // Clean up the timeout on unmount
+      };
+    }
+  }, [calendarRef]);
+
+ 
+
+  function dayCellContent() {
+    // Get the week row element
+    const weekRow = document.querySelector('tr');
+
+    // Check if the week row element exists
+    if (weekRow) {
+
+      const weekRow = document.querySelector('.fc-scrollgrid-section-body .fc-daygrid-body-unbalanced table tbody tr');
+      const dayElements = weekRow.querySelectorAll('td[data-date]');
+
+      dayElements.forEach((dayElement) => {
+        const date = dayElement.getAttribute('data-date');
+        const dayOfMonth = new Date(date).getDate();
+        const dayOfWeek = new Date(date).toLocaleString('en-us', { weekday: 'short' });
+
+        // Add day of the month on the top left corner
+        const dayOfMonthElement = document.createElement('div');
+        dayOfMonthElement.className = 'day-of-month';
+        dayOfMonthElement.innerHTML = String(dayOfMonth);
+        dayElement.querySelector('.fc-daygrid-day-frame').prepend(dayOfMonthElement);
+
+        // Add name of the day of the week on the top right corner
+        const dayOfWeekElement = document.createElement('div');
+        dayOfWeekElement.className = 'day-of-week';
+        dayOfWeekElement.innerHTML = dayOfWeek;
+        dayElement.querySelector('.fc-daygrid-day-frame').appendChild(dayOfWeekElement);
+
+
+        // 3. Add counter on the left bottom corner
+        const counterElement = document.createElement('div');
+        counterElement.className = 'counter';
+        counterElement.innerHTML = '0/100';
+        dayElement.querySelector('.fc-daygrid-day-frame').prepend(counterElement);
+
+        // 4. Add small icon chevron button on the bottom right corner
+        const chevronContainer = `
+          <div class='chevron'>
+            <button key='${date}' id='btn-${date}' class='chevron-btn'>
+              <span>✔</span>
+            </button>
+          </div>`;
+
+        // const chevronElement = document.createElement('div');
+        // chevronElement.className = 'chevron';
+        // dayElement.querySelector('.fc-daygrid-day-bottom').prepend(chevronElement);
+        dayElement.querySelector('.fc-daygrid-day-frame').insertAdjacentHTML('beforeend', chevronContainer);
+
+        const buttons = document.querySelectorAll('.chevron-btn');
+
+        buttons.forEach(button => {
+          button.addEventListener('click', () => {
+            button.classList.add('active');
+          });
+        });
+      })
+    }
+    else console.error('Failed to find the element!');
+  };
+
+  return (
+    <div>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+          <div
+            ref={calendarRef}
+            className={'fcContainer'}
+            style={{ maxHeight: '30vh', margin: '20px 0px' }}
+          />
+        </div>
+      )}
+      {/* <div>Last Updated: {}</div> */}
+    </div>
+  );
+}
+
 const LnsBookingsProps: React.FC<ILnsBookingsProps> = (props) => {
   const {
-      // @ts-ignore
+    // @ts-ignore
     description,
-      // @ts-ignore
+    // @ts-ignore
     isDarkTheme,
-      // @ts-ignore
+    // @ts-ignore
     environmentMessage,
     hasTeamsContext,
     userDisplayName,
@@ -25,13 +148,14 @@ const LnsBookingsProps: React.FC<ILnsBookingsProps> = (props) => {
 
   // @ts-ignore
   const [selectedUsers, setSelectedUsers] = useState<IPersonaProps[]>([]);
+
   // @ts-ignore
   const [currentUserType, setCurrentUserType] = useState<string>('admin');
 
   useEffect(() => {
     const getCurrentUser = async () => {
       const endpoint: string = `${currentSiteUrl}/_api/web/currentUser/?$select=IsSiteAdmin`;
-  
+
       try {
         const response = await spHttpClient.post(
           endpoint,
@@ -40,24 +164,24 @@ const LnsBookingsProps: React.FC<ILnsBookingsProps> = (props) => {
             headers: [['accept', 'application/json;odata.metadata=none']],
           }
         );
-  
-       // const response: SPHttpClientResponse = await spHttpClient.get(endpoint, SPHttpClient.configurations.v1, requestOptions);
+
+        // const response: SPHttpClientResponse = await spHttpClient.get(endpoint, SPHttpClient.configurations.v1, requestOptions);
         const user: any = await response.json();
-        try{
+        try {
           console.log('Allow Invite Response --', user)
           setCurrentUserType(user.IsSiteAdmin ? 'admin' : 'user');
-          
-        } catch(error){
+
+        } catch (error) {
           console.log(error);
         }
-       
+
       } catch (error) {
         console.error('Error retrieving user information:', error);
       }
     };
-  
+
     getCurrentUser();
-  
+
   }, [currentSiteUrl, spHttpClient]);
 
   const onResolveSuggestions = async (
@@ -77,7 +201,7 @@ const LnsBookingsProps: React.FC<ILnsBookingsProps> = (props) => {
         SharePointGroupID: 0,
         UrlZone: 1
       };
-     
+
       let parsedValue;
       const response = await spHttpClient.post(
         endpoint,
@@ -141,21 +265,24 @@ const LnsBookingsProps: React.FC<ILnsBookingsProps> = (props) => {
         {/* TODO - change this to === / !== in order to make it correct */}
 
         {currentUserType !== 'admin' && (
-        <NormalPeoplePicker
-          onResolveSuggestions={onResolveSuggestions}
-          onChange={onChange}
-          selectedItems={selectedUsers}
-          resolveDelay={1000}
-        />
+          <NormalPeoplePicker
+            onResolveSuggestions={onResolveSuggestions}
+            onChange={onChange}
+            selectedItems={selectedUsers}
+            resolveDelay={1000}
+          />
         )}
 
         {currentUserType === 'admin' && (
           <><span aria-disabled>no permissions to invite users</span></>
-        )}  
-
+        )}
+        <LnsCalendar />
       </div>
     </section>
   );
 };
 
 export default LnsBookingsProps;
+
+
+
